6409660f9baf328a4372f9c4f25f339c7bdf4c29
Kamalarani Pichaimuneswaran <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-04-13 15:06:04 +0530

    addind Autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    console.log("Should scroll");
+  }
+
+}
+
 socket.on("connect",function(){
   console.log("Connected To Server");
 });
@@ -18,7 +33,7 @@ socket.on("newMessage",function(message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 /*socket.emit("createMessage", {
@@ -43,6 +58,7 @@ socket.on("newLocationMessage",function(message){
   a.attr('href',message.url);
   li.append(a);*/
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit' , function(e){
